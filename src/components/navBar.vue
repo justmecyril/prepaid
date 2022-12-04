3084f4d25bd0165a208daf11efcf333217a7368d
justmecyril <kofisem@gmail.com>
2022-12-04 17:23:44 +0000

    fixes: navbar buttons

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="top-0 z-50 bg-gray-100 h-22 fixed w-screen opacity-90">
+  <div class="fixed top-0 z-50 w-screen bg-gray-100 h-22 opacity-90">
     <nav class="container px-8 py-5 mx-auto">
       <div class="flex justify-between">
         <!-- Left elements -->
@@ -19,15 +19,7 @@
               <button
                 type="submit"
                 id="searchsubmit"
-                class="
-                  absolute
-                  inset-y-0
-                  left-0
-                  flex
-                  items-center
-                  pl-3
-                  pointer-events-none
-                "
+                class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none "
               >
                 <svg
                   class="w-5 h-5 text-gray-400"
@@ -45,24 +37,7 @@
                 type="text"
                 name="s"
                 id="s"
-                class="
-                  block
-                  w-full
-                  py-2
-                  pl-10
-                  pr-3
-                  leading-5
-                  text-gray-300
-                  placeholder-gray-400
-                  transition
-                  duration-150
-                  ease-in-out
-                  bg-white
-                  border border-transparent
-                  rounded-md
-                  focus:outline-none focus:bg-white focus:text-gray-900
-                  sm:text-sm
-                "
+                class="block w-full py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 transition duration-150 ease-in-out bg-white border border-transparent rounded-md focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm"
                 placeholder="Search"
               />
             </form>
@@ -84,9 +59,10 @@
                 rounded-md
                 btn-secondary
               "
-              @click.prevent="toggleSignInModal"
+              @click.prevent="isOpen = true"
               >Sign in</a
             >
+          
             <a
               href="#"
               class="
@@ -105,16 +81,7 @@
             <div class="mr-8"><h3>Contracts</h3></div>
             <div class="relative mt-2 mr-2 dropdown">
               <a
-                class="
-                  flex
-                  items-center
-                  mr-4
-                  text-gray-500
-                  hover:text-gray-700
-                  focus:text-gray-700
-                  dropdown-toggle
-                  hidden-arrow
-                "
+                class="flex items-center mr-4 text-gray-500 hover:text-gray-700 focus:text-gray-700 dropdown-toggle hidden-arrow"
                 href="#"
                 id="dropdownMenuButton1"
                 role="button"
@@ -149,85 +116,30 @@
                     py-0
                     px-1.5
                   "
-                  >1</span
+                  ></span
                 >
               </a>
               <ul
-                class="
-                  absolute
-                  right-0
-                  left-auto
-                  z-50
-                  hidden
-                  float-left
-                  py-2
-                  m-0
-                  mt-1
-                  text-base text-left
-                  list-none
-                  bg-white
-                  border-none
-                  rounded-lg
-                  shadow-lg
-                  dropdown-menu
-                  min-w-max
-                  bg-clip-padding
-                "
+                class="absolute right-0 left-auto z-50 hidden float-left py-2 m-0 mt-1 text-base text-left list-none bg-white border-none rounded-lg shadow-lg dropdown-menu min-w-max bg-clip-padding"
                 aria-labelledby="dropdownMenuButton1"
               >
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Another action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Something else here</a
                   >
@@ -244,7 +156,7 @@
                 aria-expanded="false"
               >
                 <img
-                  src="https://mdbootstrap.com/img/new/avatars/2.jpg"
+                  src=""
                   class="rounded-full"
                   style="height: 25px; width: 25px"
                   alt=""
@@ -252,81 +164,26 @@
                 />
               </a>
               <ul
-                class="
-                  absolute
-                  right-0
-                  left-auto
-                  z-50
-                  hidden
-                  float-left
-                  py-2
-                  m-0
-                  mt-1
-                  text-base text-left
-                  list-none
-                  bg-white
-                  border-none
-                  rounded-lg
-                  shadow-lg
-                  dropdown-menu
-                  min-w-max
-                  bg-clip-padding
-                "
+                class="absolute right-0 left-auto z-50 hidden float-left py-2 m-0 mt-1 text-base text-left list-none bg-white border-none rounded-lg shadow-lg dropdown-menu min-w-max bg-clip-padding"
                 aria-labelledby="dropdownMenuButton2"
               >
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Another action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="
-                      block
-                      w-full
-                      px-4
-                      py-2
-                      text-sm
-                      font-normal
-                      text-gray-700
-                      bg-transparent
-                      dropdown-item
-                      whitespace-nowrap
-                      hover:bg-gray-100
-                    "
+                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
                     href="#"
                     >Something else here</a
                   >
@@ -341,19 +198,10 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+
+
 export default {
-  name: "navBar",
-  methods: {
-    ...mapMutations(["toggleSignInModal"]),
-    // toggleSignInModal() {
-    //   this.$store.commit("toggleSignInModal");
-    // },
-  },
+ 
+
 };
 </script>
-<style scoped>
-.dropdown:hover .dropdown-menu {
-  display: block;
-}
-</style>
