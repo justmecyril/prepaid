5587a2e2d3a2cf16fc996324debb45510fb6c46a
justmecyril <kofisem@gmail.com>
2022-12-04 10:36:54 +0000

    feat: navbar implementation

--- src/components/Home/HeroView.vue
+++ src/components/Home/HeroView.vue
@@ -1,20 +1,53 @@
 <template>
-        <div>
-        <div class="relative">
-          <img src="@/assets/images/aerial-view-of-accra.jpeg" class="object-cover w-full h-[96]" />
-          <div class="w-full"/>
-          <h1 class="absolute text-3xl font-bold text-green-900 top-32 left-24">Your Digital Realtor, we pick <br>up where your realtor/agent leaves you.</h1>
-          <p class="absolute my-4 text-green-900 top-48 left-24">Seamlessly generate, customise and co-ordinate signing of your real estate agreements</p>
-             <a
-              href="#"
-              class="absolute px-4 py-2 text-sm font-semibold text-white capitalize bg-green-900 border rounded-lg bottom-24 "
-              >Get Started</a
-            >
-        </div>
-      </div>
-      <div class="h-20 bg-gray-200">
-        <p class="pt-8 text-center text-green-900">
-          Dealtor simplifies the contracting phase of real estate transactions
-        </p>
-      </div>
+  <div>
+    <div class="relative">
+      <img
+        src="@/assets/images/aerial-view-neighborhood.jpeg"
+        class="object-cover w-full h-[27rem]"
+      />
+      <div
+        class="
+          absolute
+          w-full
+          h-[27rem]
+          bg-gradient-to-tr
+          from-white
+          to-white
+          opacity-70
+          bottom-0
+          left-0
+          right-0
+        "
+      />
+      <h1 class="absolute text-4xl font-bold text-green-900 top-[12rem] left-24">
+        Your Digital Realtor, we pick <br />up where your realtor/ agent leaves
+        you.
+      </h1>
+      <p class="absolute my-4 text-green-900 top-[16rem] left-24 font-light">
+        Seamlessly generate, customise and co-ordinate signing of your real
+        estate agreements
+      </p>
+      <a
+        href="#"
+        class="
+          absolute
+          px-6
+          py-2
+          text-[.5rem]
+          font-semibold
+          capitalize
+          rounded-sm
+          bottom-20
+          left-24
+          btn-primary
+        "
+        >Get Started</a
+      >
+    </div>
+  </div>
+  <div class="h-20 bg-gray-200">
+    <p class="pt-8 text-center text-green-900">
+      Dealtor simplifies the contracting phase of real estate transactions
+    </p>
+  </div>
 </template>
--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="sticky top-0 z-50 bg-gray-100 h-22">
-    <nav class="container px-4 py-5 mx-auto">
+  <div class="top-0 z-50 bg-gray-100 h-22 fixed w-screen opacity-90">
+    <nav class="container px-8 py-5 mx-auto">
       <div class="flex justify-between">
         <!-- Left elements -->
         <div class="flex">
           <router-link
-            :to="{ name: 'Home' }"
+            :to="{ name: 'HomeView' }"
             class="mr-4 text-2xl font-normal text-green-900 capitalize"
             >Dealtor<span class="font-bold text-yellow-500"
               >.</span
@@ -19,7 +19,15 @@
               <button
                 type="submit"
                 id="searchsubmit"
-                class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none "
+                class="
+                  absolute
+                  inset-y-0
+                  left-0
+                  flex
+                  items-center
+                  pl-3
+                  pointer-events-none
+                "
               >
                 <svg
                   class="w-5 h-5 text-gray-400"
@@ -37,7 +45,24 @@
                 type="text"
                 name="s"
                 id="s"
-                class="block w-full py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 transition duration-150 ease-in-out bg-white border border-transparent rounded-md focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm"
+                class="
+                  block
+                  w-full
+                  py-2
+                  pl-10
+                  pr-3
+                  leading-5
+                  text-gray-300
+                  placeholder-gray-400
+                  transition
+                  duration-150
+                  ease-in-out
+                  bg-white
+                  border border-transparent
+                  rounded-md
+                  focus:outline-none focus:bg-white focus:text-gray-900
+                  sm:text-sm
+                "
                 placeholder="Search"
               />
             </form>
@@ -49,13 +74,30 @@
           <div class="mr-8">
             <a
               href="#"
-              class="px-4 py-2 mr-4 text-sm font-semibold capitalize rounded-md btn-secondary"
+              class="
+                px-4
+                py-2
+                mr-4
+                text-[.8rem]
+                font-light
+                capitalize
+                rounded-md
+                btn-secondary
+              "
               @click.prevent="toggleSignInModal"
               >Sign in</a
             >
             <a
               href="#"
-              class="px-4 py-2 text-sm font-semibold capitalize rounded-md btn-primary"
+              class="
+                px-4
+                py-2
+                text-[.8rem]
+                font-light
+                capitalize
+                rounded-md
+                btn-primary
+              "
               >Sign up</a
             >
           </div>
@@ -63,7 +105,16 @@
             <div class="mr-8"><h3>Contracts</h3></div>
             <div class="relative mt-2 mr-2 dropdown">
               <a
-                class="flex items-center mr-4 text-gray-500 hover:text-gray-700 focus:text-gray-700 dropdown-toggle hidden-arrow"
+                class="
+                  flex
+                  items-center
+                  mr-4
+                  text-gray-500
+                  hover:text-gray-700
+                  focus:text-gray-700
+                  dropdown-toggle
+                  hidden-arrow
+                "
                 href="#"
                 id="dropdownMenuButton1"
                 role="button"
@@ -102,26 +153,81 @@
                 >
               </a>
               <ul
-                class="absolute right-0 left-auto z-50 hidden float-left py-2 m-0 mt-1 text-base text-left list-none bg-white border-none rounded-lg shadow-lg dropdown-menu min-w-max bg-clip-padding"
+                class="
+                  absolute
+                  right-0
+                  left-auto
+                  z-50
+                  hidden
+                  float-left
+                  py-2
+                  m-0
+                  mt-1
+                  text-base text-left
+                  list-none
+                  bg-white
+                  border-none
+                  rounded-lg
+                  shadow-lg
+                  dropdown-menu
+                  min-w-max
+                  bg-clip-padding
+                "
                 aria-labelledby="dropdownMenuButton1"
               >
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Another action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Something else here</a
                   >
@@ -146,26 +252,81 @@
                 />
               </a>
               <ul
-                class="absolute right-0 left-auto z-50 hidden float-left py-2 m-0 mt-1 text-base text-left list-none bg-white border-none rounded-lg shadow-lg dropdown-menu min-w-max bg-clip-padding"
+                class="
+                  absolute
+                  right-0
+                  left-auto
+                  z-50
+                  hidden
+                  float-left
+                  py-2
+                  m-0
+                  mt-1
+                  text-base text-left
+                  list-none
+                  bg-white
+                  border-none
+                  rounded-lg
+                  shadow-lg
+                  dropdown-menu
+                  min-w-max
+                  bg-clip-padding
+                "
                 aria-labelledby="dropdownMenuButton2"
               >
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Another action</a
                   >
                 </li>
                 <li>
                   <a
-                    class="block w-full px-4 py-2 text-sm font-normal text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100"
+                    class="
+                      block
+                      w-full
+                      px-4
+                      py-2
+                      text-sm
+                      font-normal
+                      text-gray-700
+                      bg-transparent
+                      dropdown-item
+                      whitespace-nowrap
+                      hover:bg-gray-100
+                    "
                     href="#"
                     >Something else here</a
                   >
